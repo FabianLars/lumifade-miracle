56f60427db43a256a14dd89616553cee6c342f46
FabianLars <FabianLars@vivaldi.net>
2020-01-03 22:28:53 +0100

    x

---
+++ FabianLars/Door.gd
@@ -0,0 +1,19 @@
+extends Area
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_Area_body_entered(body):
+	if body.name == "Player" || body.name == "Gary" || body.name == "Character":
+		if self.name == "AreaDF":
+			Global.goto_scene("res://FabianLars/Main.tscn")
+
+		elif self.name == "AreaDD":
+			Global.goto_scene("res://Deniz/Level.tscn")
+
+		elif self.name == "AreaDM":
+			Global.goto_scene("res://Mike/Main.tscn")
+
+		elif self.name == "AreaDL":
+			Global.goto_scene("res://Lukas/Main.tscn")
