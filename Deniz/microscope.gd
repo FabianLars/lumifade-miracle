cccf3ac9888a4f6814531c569f1300f5015a8e11
DenizGurbey <denizguerbey98@gmail.com>
2020-01-09 11:31:48 +0100

    unvollständige branch

---
+++ Deniz/microscope.gd
@@ -0,0 +1,34 @@
+extends MeshInstance
+
+var button=null
+var texture=null
+
+func _ready():
+	texture=get_parent().get_node("MicroRect")
+	#button=get_parent().get_node("MicroRect/MicroButton")
+	
+	
+func on_button_pressed():
+
+	get_parent().get_node("Micro_Camera").current=false
+	get_parent().get_node("Player/Head/Camera").current=true
+	get_tree().paused=false
+	#texture.visible=false
+	
+	
+func _process(delta):
+	if Input.is_action_just_pressed("ui_Z"):
+		if(get_translation().distance_to(get_parent().get_node("Player").get_translation())<4):
+			pause_mode = Node.PAUSE_MODE_PROCESS
+			#button.connect("pressed",self,"on_button_pressed")
+#			texture.visible=not texture.visible
+
+			get_parent().get_node("Micro_Camera").current=not get_parent().get_node("Micro_Camera").current
+			get_parent().get_node("Camera_Board").current=false 
+			#get_parent().get_node("Player/Head/Camera").current=not get_parent().get_node("Player/Head/Camera").current
+			get_tree().paused=not get_tree().paused
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+				
+			
+		else:
+			pause_mode = Node.PAUSE_MODE_INHERIT
